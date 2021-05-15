a20c0e4a664ea15a532ae6367fd7ae31c4307168
yao <yyaowill@gmail.com>
2021-05-15 17:18:50 +0100

    Q1: Fixed gap to be between [1, 3, 7, 15, 31, 63, 127, 255, 511] - tidy up tests

--- src/main/java/Q1/CWK2Q1.java
+++ src/main/java/Q1/CWK2Q1.java
@@ -20,8 +20,11 @@ import java.util.*;
 public class CWK2Q1 {
     public static void shell_sort(ArrayList<Double> array) {
         int n = array.size();
+        List<Integer> gaps = Arrays.asList(1, 3, 7, 15, 31, 63, 127, 255, 511);
+        gaps.sort(Comparator.reverseOrder());
 
-        for (int gap = n / 2; gap > 0; gap /= 2) {
+//        for (int gap = n / 2; gap > 0; gap /= 2) {
+        for ( int gap: gaps) {
             for (int i = gap; i < n; i += 1) {
                 double temp = array.get(i);
                 int j;
@@ -29,38 +32,26 @@ public class CWK2Q1 {
                     array.set(j, array.get(j - gap));
                 array.set(j, temp);
             }
-            System.out.println(array);
+//            System.out.println(array);
         }
-//        System.out.print(array);
+        System.out.print(array);
     }
-
 
     public static void main(String[] args) {
-//        ArrayList<Double> testList = new ArrayList<Double>();
-//        testList.add(3.4);
-//        testList.add(6.55);
-//        testList.add(-12.2);
-//        testList.add(1.73);
-//        testList.add(140.98);
-//        testList.add(-4.18);
-//        testList.add(52.87);
-//        testList.add(99.14);
-//        testList.add(73.202);
-//        testList.add(-23.6);
-
         ArrayList<Double> testList = new ArrayList<Double>();
+        testList.add(3.4);
+        testList.add(6.55);
+        testList.add(-12.2);
+        testList.add(1.73);
+        testList.add(140.98);
+        testList.add(-4.18);
+        testList.add(52.87);
+        testList.add(99.14);
+        testList.add(73.202);
+        testList.add(-23.6);
 
-        Random rand = new Random();
-        for (int k = 0; k < 1000; k++) {
-            int pick = rand.nextInt(88) + 13;
-            testList.add((double) pick);
-        }
-
-//        ArrayList<Double> testList = new ArrayList<Double>(convertArrayIntegerToDouble(Arrays.asList(61, 109, 149, 111, 34, 2)));
         shell_sort(testList);
     }
-
-
 
     public static List<Double> convertArrayIntegerToDouble(List<Integer> integers) {
         List<Double> doubles = new ArrayList<Double>();
--- src/test/java/Q1/CWK2Q1Test.java
+++ src/test/java/Q1/CWK2Q1Test.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -32,21 +30,11 @@ class CWK2Q1Test {
 
     @Test
     void checkFinalResultForShortIntegerArrays(){
-        List<Double> expectedSorted = convertArrayIntegerToDouble(Arrays.asList(2, 24, 27, 34, 61, 109, 111, 119, 122, 125, 145, 149));
-        CWK2Q1.shell_sort(new ArrayList<Double>(convertArrayIntegerToDouble(Arrays.asList(61, 109, 149, 111, 34, 2, 24, 119, 122, 125, 27, 145))));
+        List<Double> expectedSorted = CWK2Q1.convertArrayIntegerToDouble(Arrays.asList(2, 24, 27, 34, 61, 109, 111, 119, 122, 125, 145, 149));
+        CWK2Q1.shell_sort(new ArrayList<Double>(CWK2Q1.convertArrayIntegerToDouble(Arrays.asList(61, 109, 149, 111, 34, 2, 24, 119, 122, 125, 27, 145))));
 
         assertThat(outContent.toString()).isEqualTo(expectedSorted.toString());
     }
-
-    public List<Double> convertArrayIntegerToDouble(List<Integer> integers) {
-        List<Double> doubles = new ArrayList<Double>();
-        for (Integer i: integers) {
-            Double d = new Double(i);
-            doubles.add(d);
-        }
-        return doubles;
-    }
-
 
     @Test
     void checkFinalResultForShortDoubleArrays(){
@@ -54,5 +42,20 @@ class CWK2Q1Test {
         CWK2Q1.shell_sort(new ArrayList<Double>(Arrays.asList(3.4, 6.55, -12.2, 1.73, 140.98, -4.18, 52.87, 99.14, 73.202, -23.6)));
 
         assertThat(outContent.toString()).isEqualTo(expectedSorted.toString());
+    }
+
+    @Test
+    void checkFinalResultForLongIntegerArrays(){
+        ArrayList<Double> testList = new ArrayList<Double>();
+        Random rand = new Random();
+        for (int k = 0; k < 1000; k++) {
+            int pick = rand.nextInt(88) + 13;
+            testList.add((double) pick);
+        }
+
+        CWK2Q1.shell_sort(testList);
+
+        testList.sort(Double::compareTo);
+        assertThat(outContent.toString()).isEqualTo(testList.toString());
     }
 }
