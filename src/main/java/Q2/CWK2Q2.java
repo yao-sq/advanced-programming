3d11c1865f1175d7603d3641d35ad96d3123b738
yao <yyaowill@gmail.com>
2021-05-16 16:21:17 +0100

    Q2: Tidy up

--- src/main/java/Q2/CWK2Q2.java
+++ src/main/java/Q2/CWK2Q2.java
@@ -1,9 +1,10 @@
 package Q2;
 
-import java.util.*;
-import java.lang.Math;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Objects;
 import java.util.function.BiFunction;
-import java.util.function.BiPredicate;
 
 /**
  * @author Anonymous (do not change)
@@ -20,33 +21,30 @@ public class CWK2Q2<T> {
     public static int interpolation_search(ArrayList<String> array, String item) {
         Collections.sort(array);
         System.out.println("Using interpolation search");
-//        return interpolation_search(array, item, CWK2Q2::characterSumDifference);
         return interpolation_search(array, item, CWK2Q2::scaledCharDifference);
     }
 
-    public static <T extends Comparable<T>> int interpolation_search(ArrayList<T> array, T item, BiFunction<T, T, Integer> minus){
+    public static <T extends Comparable<T>> int interpolation_search(ArrayList<T> array, T item, BiFunction<T, T, Integer> minus) {
         return interpolation_search(array, item, minus, Comparator.naturalOrder());
     }
 
-    public static <T> int interpolation_search( ArrayList<T> array, T item, BiFunction<T, T, Integer> minus, Comparator<T> comparator ){
+    public static <T> int interpolation_search(ArrayList<T> array, T item, BiFunction<T, T, Integer> minus, Comparator<T> comparator) {
         int low = 0;
-        int high = array.size() -1;
+        int high = array.size() - 1;
         int mid;
 
-        while ( !Objects.equals(array.get(high), array.get(low))
-                  && comparator.compare(item, array.get(low)) >=0
-                  && comparator.compare(item, array.get(high)) <=0
-        ){
+        while (!Objects.equals(array.get(high), array.get(low))
+                && comparator.compare(item, array.get(low)) >= 0
+                && comparator.compare(item, array.get(high)) <= 0
+        ) {
             mid = low + (minus.apply(item, array.get(low))
-                    * ((high-low) / minus.apply(array.get(high), array.get(low))));
+                    * ((high - low) / minus.apply(array.get(high), array.get(low))));
 
             if (comparator.compare(array.get(mid), item) < 0) {
                 low = mid + 1;
-            }
-            else if (comparator.compare(item, array.get(mid)) < 0){
-                high = mid -1;
-            }
-            else {
+            } else if (comparator.compare(item, array.get(mid)) < 0) {
+                high = mid - 1;
+            } else {
                 return mid;
             }
         }
@@ -58,46 +56,15 @@ public class CWK2Q2<T> {
         return -1;
     }
 
-
     public static int scaledCharDifference(String a, String b) {
         int commonLength = Math.min(a.length(), b.length());
-        for (int i = 0; i<commonLength; i++) {
+        for (int i = 0; i < commonLength; i++) {
             int diff = Character.compare(a.charAt(i), b.charAt(i));
             if (diff != 0) {
                 return diff * (82595524 - i);
             }
         }
         return Integer.compare(a.length(), b.length());
-    }
-
-
-    public static int characterSumDifference(String a, String b){
-        return characterSum(a) - characterSum(b);
-    }
-    private static int characterSum(String a) {
-        return a.chars().sum();
-    }
-
-
-    public static int commonLengthCharacterSumDifference(String a, String b){
-        int length = Math.min(a.length(), b.length());
-        String subtractor = a.substring(0,length);
-        String subtractee = b.substring(0,length);
-        return convertWord(subtractor) - convertWord(subtractee);
-    }
-
-    public static int convertString(String s) {
-        return s.toLowerCase().hashCode() - 96;
-    }
-
-    public static int convertWord(String s) {
-        int length = s.length();
-        double sum = 0;
-        for (int i = 0; i < length; i++) {
-            int value = convertString(s.substring(i, i + 1));
-            sum = sum + value * Math.pow(10.0, length - i - 1);
-        }
-        return (int) sum;
     }
 
     public static void main(String[] args) {
--- src/test/java/Q2/CWK2Q2Test.java
+++ src/test/java/Q2/CWK2Q2Test.java
@@ -1,56 +1,42 @@
 package Q2;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.*;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CWK2Q2Test {
     @Test
-    public void interpolationSearch_example1(){
+    public void interpolationSearch_example1() {
         List<String> words = asList("Hello", "World", "How", "Are", "You");
         ArrayList<String> array = new ArrayList<>(words);
 
-        assertThat( CWK2Q2.interpolation_search(array, "Are")).isEqualTo(0);
-        assertThat( CWK2Q2.interpolation_search(array, "Hello")).isEqualTo(1);
-        assertThat( CWK2Q2.interpolation_search(array, "How")).isEqualTo(2);
-        assertThat( CWK2Q2.interpolation_search(array, "World")).isEqualTo(3);
-        assertThat( CWK2Q2.interpolation_search(array, "You")).isEqualTo(4);
+        assertThat(CWK2Q2.interpolation_search(array, "Are")).isEqualTo(0);
+        assertThat(CWK2Q2.interpolation_search(array, "Hello")).isEqualTo(1);
+        assertThat(CWK2Q2.interpolation_search(array, "How")).isEqualTo(2);
+        assertThat(CWK2Q2.interpolation_search(array, "World")).isEqualTo(3);
+        assertThat(CWK2Q2.interpolation_search(array, "You")).isEqualTo(4);
     }
 
     @Test
-    public void interpolationSearch_example2(){
+    public void interpolationSearch_example2() {
         List<String> words = asList("Ma", "Mar", "Mary", "Marianne", "Marianna", "Mariann", "Rosymary");
         ArrayList<String> array = new ArrayList<>(words);
 
-        assertThat( CWK2Q2.interpolation_search(array, "Ma")).isEqualTo(0);
-        assertThat( CWK2Q2.interpolation_search(array, "Mar")).isEqualTo(1);
-        assertThat( CWK2Q2.interpolation_search(array, "Mariann")).isEqualTo(2);
-        assertThat( CWK2Q2.interpolation_search(array, "Marianna")).isEqualTo(3);
-        assertThat( CWK2Q2.interpolation_search(array, "Marianne")).isEqualTo(4);
-        assertThat( CWK2Q2.interpolation_search(array, "Mary")).isEqualTo(5);
-        assertThat( CWK2Q2.interpolation_search(array, "Rosymary")).isEqualTo(6);
-    }
-
-
-
-
-    @Test
-    void checkConvertedLetterHasRightValue(){
-        String s = "a";
-        int expected = s.toLowerCase().hashCode() - 96;
-        int actual = CWK2Q2.convertString(s);
-
-        assertThat(actual).isEqualTo(expected);
+        assertThat(CWK2Q2.interpolation_search(array, "Ma")).isEqualTo(0);
+        assertThat(CWK2Q2.interpolation_search(array, "Mar")).isEqualTo(1);
+        assertThat(CWK2Q2.interpolation_search(array, "Mariann")).isEqualTo(2);
+        assertThat(CWK2Q2.interpolation_search(array, "Marianna")).isEqualTo(3);
+        assertThat(CWK2Q2.interpolation_search(array, "Marianne")).isEqualTo(4);
+        assertThat(CWK2Q2.interpolation_search(array, "Mary")).isEqualTo(5);
+        assertThat(CWK2Q2.interpolation_search(array, "Rosymary")).isEqualTo(6);
     }
 
     @Test
@@ -59,11 +45,8 @@ class CWK2Q2Test {
         String[] words = getStringFromFile();
 
         ArrayList<String> testList = new ArrayList<String>();
-        for ( String word: words){
-            testList.add(word);
-        }
+        testList.addAll(asList(words));
 
-        //FIXME:
         assertThat(CWK2Q2.interpolation_search(testList, item)).isEqualTo(3362);
     }
 
@@ -76,22 +59,16 @@ class CWK2Q2Test {
     }
 
     public String[] getStringFromFile() throws FileNotFoundException {
-        File file=new File("/Users/yao/IdeaProjects/advanced-programming/src/main/java/Q2/names.txt");
-        Scanner sc=new Scanner(file);
+        File file = new File("/Users/yao/IdeaProjects/advanced-programming/src/main/java/Q2/names.txt");
+        Scanner sc = new Scanner(file);
         sc.useDelimiter("\\Z");
         String s = sc.next();
         String[] words = s.split(",");
 
         System.out.println("Total count is: " + words.length);
-        long uniqueCount = Arrays.stream(words).distinct().count();
-        System.out.println("Unique count is: " + uniqueCount);
-        Arrays.setAll(words, i -> words[i].substring(1, words[i].length()-1));
+        System.out.println("Unique count is: " + Arrays.stream(words).distinct().count());
+        Arrays.setAll(words, i -> words[i].substring(1, words[i].length() - 1));
 //        Arrays.setAll(words, i -> words[i].replaceFirst("^\"(.*)\"$", "$1"));
         return words;
-    }
-
-    @Test
-    void check(){
-        System.out.println(CWK2Q2.scaledCharDifference("Mar", "Mary"));
     }
 }
