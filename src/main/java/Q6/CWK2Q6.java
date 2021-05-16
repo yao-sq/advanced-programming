323f6ab026e655158c4fe7c9ee7c3db200c15ff8
yao <yyaowill@gmail.com>
2021-05-16 17:07:09 +0100

    Q6: Renames, tidy up, MVP ready for short example

--- src/main/java/Q6/CWK2Q6.java
+++ src/main/java/Q6/CWK2Q6.java
@@ -2,152 +2,149 @@ package Q6;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 import static java.util.Arrays.asList;
 
 /**
- *  @author Anonymous (do not change)
- *
- *  Question 6:
- *
- *  Implement in Java a similar algorithm to that in Q5, i.e. given a
- *  block of text your algorithm should be able to redact words from
- *  a given set and outputs the result to a file called â€œresult.txtâ€�.
- *  However, in this implementation of the algorithm all
- *  redactable words will be proper nouns (i.e. a name used for an
- *  individual person, place, or organisation, spelled with an initial
- *  capital letter) and your algorithm should take into account that
- *  the list of redactable words might not be complete. For example,
- *  given the block of text:
- *      It was in July, 1805, and the speaker was the well-known Anna
- *      Pavlovna Scherer, maid of honor and favorite of the Empress
- *      Marya Fedorovna. With these words she greeted Prince Vasili
- *      Kuragin, a man of high rank and importance, who was the first
- *      to arrive at her reception. Anna Pavlovna had had a cough for
- *      some days. She was, as she said, suffering from la grippe;
- *      grippe being then a new word in St. Petersburg, used only by
- *      the elite.
- *
- *  and the redactable set of words
- *      Anna Pavlovna Scherer, St. Petersburg, Marya Fedorovna
- *
- *  the output text should be
- *      It was in ****, 1805, and the speaker was the well-known ****
- *      ******** *******, maid of honor and favorite of the *******
- *      ***** *********. With these words she greeted ****** ******
- *      *******, a man of high rank and importance, who was the first
- *      to arrive at her reception. **** ******** had had a cough for
- *      some days. She was, as she said, suffering from la grippe;
- *      grippe being then a new word in *** **********, used only by
- *      the elite.
- *
- *  You should test your program using the example files provided.
-*/
+ * @author Anonymous (do not change)
+ * <p>
+ * Question 6:
+ * <p>
+ * Implement in Java a similar algorithm to that in Q5, i.e. given a
+ * block of text your algorithm should be able to redact words from
+ * a given set and outputs the result to a file called â€œresult.txtâ€�.
+ * However, in this implementation of the algorithm all
+ * redactable words will be proper nouns (i.e. a name used for an
+ * individual person, place, or organisation, spelled with an initial
+ * capital letter) and your algorithm should take into account that
+ * the list of redactable words might not be complete. For example,
+ * given the block of text:
+ * It was in July, 1805, and the speaker was the well-known Anna
+ * Pavlovna Scherer, maid of honor and favorite of the Empress
+ * Marya Fedorovna. With these words she greeted Prince Vasili
+ * Kuragin, a man of high rank and importance, who was the first
+ * to arrive at her reception. Anna Pavlovna had had a cough for
+ * some days. She was, as she said, suffering from la grippe;
+ * grippe being then a new word in St. Petersburg, used only by
+ * the elite.
+ * <p>
+ * and the redactable set of words
+ * Anna Pavlovna Scherer, St. Petersburg, Marya Fedorovna
+ * <p>
+ * the output text should be
+ * It was in ****, 1805, and the speaker was the well-known ****
+ * ******** *******, maid of honor and favorite of the *******
+ * ***** *********. With these words she greeted ****** ******
+ * *******, a man of high rank and importance, who was the first
+ * to arrive at her reception. **** ******** had had a cough for
+ * some days. She was, as she said, suffering from la grippe;
+ * grippe being then a new word in *** **********, used only by
+ * the elite.
+ * <p>
+ * You should test your program using the example files provided.
+ */
 
 public class CWK2Q6 {
 
-	public static void redactWords(String textFilename, String redactWordsFilename){
-		String fileContent = readFile(textFilename);
-		String redactNounsContent = readFile(redactWordsFilename);
+    public static void redactWords(String textFilename, String redactWordsFilename) {
+        String fileContent = readFile(textFilename);
+        String redactContent = readFile(redactWordsFilename);
 
-		String[] redactNouns = redactNounsContent.split("\\s*,\\s*");
+        String[] phrases = redactContent.split("\\s*,\\s*");
 
-		String redacted = redactNouns(fileContent, asList(redactNouns));
-		System.out.println(redacted);
-	}
+        String redacted = redactNouns(fileContent, asList(phrases));
+        System.out.println(redacted);
+    }
 
-	public static String redactNouns(String text, List<String> redactNouns) {
-		ArrayList<String> redactWords = new ArrayList<>();
-		for (String noun : redactNouns) {
-			for (String redactWord : noun.split(" ")) {
-				redactWords.add(redactWord);
-			}
-		}
-		String redacted = redactWords(text, redactWords);
+    public static String redactNouns(String text, List<String> phrases) {
+        ArrayList<String> words = new ArrayList<>();
+        for (String phrase : phrases) {
+            for (String word : phrase.split(" ")) {
+                words.add(word);
+            }
+        }
+        String redacted = redactWords(text, words);
+        // Processing other proper nouns
+        redacted = redactOtherNouns(redacted);
 
+        return redacted;
+    }
 
-		// Processing other proper nouns
-		redacted = redactOtherNouns(redacted);
+    public static String redactWords(String text, List<String> words) {
+        // Processing redact words recorded in the file
+        return replaceWordsInFile(text, words);
+    }
 
-		return redacted;
-	}
+    public static String redactOtherNouns(String text) {
+        List<String> otherProperNouns = getOtherProperNouns(text);
+        return replaceWordsInFile(text, otherProperNouns);
+    }
 
-	public static String redactWords(String text, List<String> redactWords) {
-		// Processing redact words recorded in the file
-		return replaceWordsInFile(text, redactWords);
-	}
+    public static List<String> getOtherProperNouns(String fileContent) {
+        List<String> properNouns = new ArrayList<>();
 
-	public static String redactOtherNouns(String text) {
-		List<String> otherProperNouns = getOtherProperNouns(text);
-		return replaceWordsInFile(text, otherProperNouns);
-	}
+        // Break file into sentences.
+        String[] sentences = fileContent.split("\\. ");
+        for (String sentence : sentences) {
+            String[] words = sentence.split(" ");
+            for (String word : words) {
+                if (sentence.indexOf(word) != 0) {
+                    if (Character.isUpperCase(word.charAt(0))) {
+                        String processedWord = !word.contains(",") ? word : word.substring(0, word.length() - 1);
+                        properNouns.add(processedWord);
+                    }
+                }
+            }
+        }
+        return properNouns;
+    }
 
-	public static List<String> getOtherProperNouns(String fileContent) {
-		List<String> properNouns = new ArrayList<>();
+    public static String replaceWordsInFile(String fileContent, List<String> phrases) {
+        for (String phrase : phrases) {
+            int phraseLength = phrase.length();
+            if (fileContent.contains(phrase)) {
+                String replacement = "";
+                for (int i = 0; i < phraseLength; i++) {
+                    if (phrase.charAt(i) == ' ') {
+                        replacement = replacement.concat(" ");
+                    } else {
+                        replacement = replacement.concat("*");
+                    }
+                }
+                fileContent = fileContent.replace(phrase, replacement);
+            }
+        }
+        return fileContent;
+    }
 
-		// Break file into sentences.
-		String[] sentences = fileContent.split("\\. ");
-		for (String sentence : sentences) {
-			String[] words = sentence.split(" ");
-			for (String word : words) {
-				if ( sentence.indexOf(word) !=0 ) {
-					if ( Character.isUpperCase(word.charAt(0))) {
-						String processedWord = !word.contains(",") ? word: word.substring(0, word.length()-1);
-						properNouns.add(processedWord);
-					}
-				}
-			}
-		}
-		return properNouns;  //FIXME: Petersberg
-	}
+    public static String readFile(String pathName) {
+        File file = new File(pathName);
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        scanner.useDelimiter("\\Z");
+        return scanner.next();
+    }
 
-	public static String replaceWordsInFile(String fileContent, List<String> entities) {
-		for (String entity : entities) {
-			int entityLength = entity.length();
-			if (fileContent.contains(entity)) {
-				String replacement = "";
-				for (int i = 0; i < entityLength; i++) {
-					if ( entity.charAt(i) == ' ') {
-						replacement = replacement.concat(" ");
-					}
-					else {
-						replacement = replacement.concat("*");
-					}
-				}
-				fileContent = fileContent.replace(entity, replacement);
-			}
-		}
-		return fileContent;
-	}
-
-	public static String readFile(String pathName) {
-		File file = new File(pathName);
-		Scanner scanner = null;
-		try {
-			scanner = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		scanner.useDelimiter("\\Z");
-		String s = scanner.next();
-		return s;
-	}
-
-	public static void main(String[] args) {
-		// keep this here as it was from the original download
+    public static void main(String[] args) {
+        // keep this here as it was from the original download
 //		String inputFile = "./debate.txt";
 //		String redactFile = "./redact.txt";
 //		redactWords(inputFile, redactFile);
 
-		String inputFile = "/Users/yao/IdeaProjects/advanced-programming/src/main/java/Q6/exampleFile.txt";
-		String redactFile = "/Users/yao/IdeaProjects/advanced-programming/src/main/java/Q6/exampleRedact.txt";
+        String inputFile = "/Users/yao/IdeaProjects/advanced-programming/src/main/java/Q6/exampleFile.txt";
+        String redactFile = "/Users/yao/IdeaProjects/advanced-programming/src/main/java/Q6/exampleRedact.txt";
 
-		redactWords(inputFile, redactFile);
-	}
+//		String inputFile = "/Users/yao/IdeaProjects/advanced-programming/src/main/java/Q6/warandpeace.txt";
+//		String redactFile = "/Users/yao/IdeaProjects/advanced-programming/src/main/java/Q6/redact.txt";
+
+        redactWords(inputFile, redactFile);
+    }
 
 }
--- src/test/java/Q6/CWK2Q6Test.java
+++ src/test/java/Q6/CWK2Q6Test.java
@@ -1,43 +1,40 @@
 package Q6;
 
-import Q1.CWK2Q1;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestTemplate;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CWK2Q6Test {
     private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private PrintStream originalOut = System.out;
 
     @BeforeEach
-    public void setUpStreams(){
+    public void setUpStreams() {
         System.setOut(new PrintStream(outContent));
     }
 
     @AfterEach
-    public void restoreStreams(){
+    public void restoreStreams() {
         System.setOut(originalOut);
     }
 
     @Test
-    public void out(){
+    public void outputCapture() {
         System.out.print("hello");
         assertThat(outContent.toString()).isEqualTo("hello");
     }
 
     @Test
-    void redactWords() {
+    void redactWords_forShortExample() {
         String inputFile = "/Users/yao/IdeaProjects/advanced-programming/src/main/java/Q6/exampleFile.txt";
         String redactFile = "/Users/yao/IdeaProjects/advanced-programming/src/main/java/Q6/exampleRedact.txt";
         String expected = "It was in ****, 1805, and the speaker was the well-known **** ******** *******, maid of honor and favorite of the ******* ***** *********. With these words she greeted ****** ****** *******, a man of high rank and importance, who was the first to arrive at her reception. **** ******** had had a cough for some days. She was, as she said, suffering from la grippe; grippe being then a new word in *** **********, used only by the elite.";
@@ -47,34 +44,31 @@ class CWK2Q6Test {
     }
 
     @Test
-    void checkReadFile(){
-        String fileContent = CWK2Q6.readFile("/Users/yao/IdeaProjects/advanced-programming/src/main/java/Q6/exampleFile.txt");
-        System.out.println(fileContent);
-
+    void wordsToRedact_isSplitInto_phrase() {
         String redactWords = CWK2Q6.readFile("/Users/yao/IdeaProjects/advanced-programming/src/main/java/Q6/exampleRedact.txt");
-        String[] entities = redactWords.split(",");
-        for (String entity : entities) {
-            System.out.println(entity.trim());
+        String[] phrases = redactWords.split("\\s*,\\s*");
+        for (String phrase : phrases) {
+            System.out.println(phrase);
         }
-        //TODO: how do we  write test for this?
+        //TODO: how do we write test for this?
     }
 
     @Test
-    void properNouns(){
+        //FIXME: Petersberg
+    void properNouns() {
         String fileContent = CWK2Q6.readFile("/Users/yao/IdeaProjects/advanced-programming/src/main/java/Q6/exampleFile.txt");
-
+        List<String> expected = asList("Anna", "July", "Pavlovna", "Scherer", "Empress", "Marya", "Fedorovna", "Prince", "Vasili", "Kuragin", "Pavlovna", "St", "Petersberg");
         List<String> properNouns = CWK2Q6.getOtherProperNouns(fileContent);
-        for (String properNoun : properNouns) {
-            System.out.println(properNoun);
-        }
-        //TODO: what should the assertions be?
+
+        Assertions.assertThat(properNouns).containsExactlyInAnyOrderElementsOf(expected);
     }
 
     @Test
-    void replace(){
+    void replace() {
         String inputContent = "It was in July, 1805, and the speaker was the well-known Anna Pavlovna Scherer, maid of honor and favorite of the Empress Marya Fedorovna.";
         String expectedContent = "It was in July, 1805, and the speaker was the well-known **** ******** Scherer, maid of honor and favorite of the Empress Marya Fedorovna.";
-        ArrayList<String> entities = new ArrayList<>(Arrays.asList("Anna","Pavlovna"));
+
+        ArrayList<String> entities = new ArrayList<>(asList("Anna", "Pavlovna"));
         String redactedContent = CWK2Q6.replaceWordsInFile(inputContent, entities);
 
         Assertions.assertThat(redactedContent).isEqualTo(expectedContent);
